13e167d6aa2aa8351afc2b010ef507639e48a55a
Real1ty <vavro.matej3@gmail.com>
2025-11-21 20:39:00 +0100

    style: format

--- libs/utils/tests/file/file-utils.test.ts
+++ libs/utils/tests/file/file-utils.test.ts
@@ -1,5 +1,5 @@
-import { describe, expect, it, vi } from "vitest";
 import type { App } from "obsidian";
+import { describe, expect, it, vi } from "vitest";
 import {
 	backupFrontmatter,
 	extractContentAfterFrontmatter,
